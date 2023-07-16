04780e0d0086d0180f60ef5aa36ad9715bcc9c9a
kalempster <kalempster@kalempster.dev>
2023-07-16 22:09:53 +0200

    Initialized astro project migrated main screen with small changes

---
+++ index/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+        solidJs(),
+    ],
+});
