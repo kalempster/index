05ec6494600a0a9e63460252748c13911bb2ffa1
kalempster <mrpiter1337@gmail.com>
2023-07-27 20:15:25 +0200

    Added all portfolio sections

--- index/src/components/Projects.tsx
+++ index/src/components/Projects.tsx
@@ -1,47 +1,54 @@
 import { Component, For, createEffect, createSignal } from "solid-js";
 import { ProjectsButton } from "./ProjectsButton";
 import carboscan from "../assets/carboscan.svg";
+import Pokeriniee from "../assets/Pokerinee.svg";
+import mpr from "../assets/mpr.png";
+import npm from "../assets/1200px-Npm-logo.svg.png";
+
 import { Svg } from "./Svg";
 const Projects = () => {
     const [currentlySelected, setCurrentlySelected] =
         createSignal<HTMLButtonElement>();
 
-    const sections: { title: string; image: Component; description: string }[] =
-        [
-            {
-                title: "carboscan",
-                image: () => <Svg url={carboscan} class="w-1/2" />,
-                description: `carboscan is an app specifically made to reduce the carbon footprint globally.
+    const sections: {
+        title: string;
+        image: Component;
+        description: string;
+        url: string;
+    }[] = [
+        {
+            title: "carboscan",
+            image: () => <Svg url={carboscan} class="w-1/2" />,
+            description: `carboscan is an app specifically made to reduce the carbon footprint globally.
         It's purpose was to show what amount of carbon footprint do certain products
         produce, and showing their alternatives.`,
-            },
-            {
-                title: "carboscan",
-                image: () => <Svg url={carboscan} class="w-1/2" />,
-                description: `carboscan is an app specifically made to reduce the carbon footprint globally.
-        It's purpose was to show what amount of carbon footprint do certain products
-        produce, and showing their alternatives.`,
-            },
-            {
-                title: "carboscan",
-                image: () => <Svg url={carboscan} class="w-1/2" />,
-                description: `carboscan is an app specifically made to reduce the carbon footprint globally.
-        It's purpose was to show what amount of carbon footprint do certain products
-        produce, and showing their alternatives.`,
-            },
-            {
-                title: "carboscan",
-                image: () => <Svg url={carboscan} class="w-1/2" />,
-                description: `carboscan is an app specifically made to reduce the carbon footprint globally.
-        It's purpose was to show what amount of carbon footprint do certain products
-        produce, and showing their alternatives.`,
-            },
-        ];
+            url: "https://github.com/kalempster/carboscan",
+        },
+        {
+            title: "Pokeriniee",
+            image: () => <Svg url={Pokeriniee} class="w-1/2" />,
+            description: `Pokeriniee is a web app that simulates online poker with fake chips, it currently has implemented a auth system, accounts and fully featured lobbies.`,
+            url: "https://github.com/kalempster/pokerini",
+        },
+        {
+            title: "Mój przyjaciel robot",
+            image: () => <img src={mpr} alt="" class="h-full" />,
+            description: `Mój przyjaciel robot is an app that targets people with motivation loss and people who want to find a hobby. It displays different events that people can go to, scan a QR code and earn points which they can exchange for rewards.`,
+            url: "https://app.mojprzyjacielrobot.pl",
+        },
+        {
+            title: "discord.js-menu-buttons",
+            image: () => <img src={npm} alt="" class="h-full" />,
+            description: `discord.js-menu-buttons is an npm package I made to simplify creating menus with buttons in discord.js. It was a modified version of a different package that was left unmaintained.`,
+            url: "https://www.npmjs.com/package/discord.js-menu-buttons",
+        },
+    ];
 
     const [currentItem, setCurrentItem] = createSignal<{
         title: string;
         image: Component;
         description: string;
+        url: string;
     }>(sections[0]!);
 
     createEffect(() => {
@@ -50,7 +57,7 @@ const Projects = () => {
 
     return (
         <div
-            class="w-[60%] flex flex-col items-start text-white gap-5 my-32"
+            class="w-[60%] flex flex-col items-start text-white gap-5 mt-32"
             id="projects"
         >
             <div class="text-6xl font-bold text-primary">
@@ -64,7 +71,7 @@ const Projects = () => {
                 </div>
             </div>
             <div class="w-full flex justify-center">
-                <div class="w-[85%] p-4 rounded-lg flex justify-center gap-10 flex-wrap border border-red-700">
+                <div class="w-[85%] p-4 rounded-lg flex justify-center gap-10 flex-wrap border border-primary">
                     <For each={sections}>
                         {(section, i) => {
                             return (
@@ -82,10 +89,14 @@ const Projects = () => {
                     </For>
                 </div>
             </div>
-            <div class="w-full flex flex-col gap-5 items-center">
-                <div class="w-full h-32 flex justify-center items-center">
+            <div class="w-full flex flex-col gap-5 items-center h-64">
+                <a
+                    target="_blank"
+                    href={currentItem().url}
+                    class="w-full h-32 flex justify-center items-center"
+                >
                     {currentItem().image({})}
-                </div>
+                </a>
 
                 <div class="w-5/12 text-justify [text-align-last:center] text-lg">
                     {currentItem().description}
