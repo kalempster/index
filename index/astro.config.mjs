1cf1e2c00b2465465cd2bcdb0cb4dc7e49df159e
kalempster <mrpiter1337@gmail.com>
2023-07-29 11:43:29 +0200

    Final deploy configuration

--- index/astro.config.mjs
+++ index/astro.config.mjs
@@ -4,6 +4,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://kalempster.dev",
     integrations: [
         tailwind({
             config: {
